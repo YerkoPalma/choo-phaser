ccdf270f36b91c6856b3bb10d5df36c0be9717cf
Yerko Palma <yerko.palma@usach.cl>
2017-10-11 13:03:14 +0000

    style tweaks and game clear

--- index.js
+++ index.js
@@ -6,7 +6,14 @@ var css = require('sheetify')
 css('tachyons', { global: true })
 
 var app = choo()
+app.use(clearGame)
 app.route('/', homeView)
 app.route('/stage/:id', stageView)
 var tree = app.start()
 document.body.appendChild(tree)
+
+function clearGame (state, emitter) {
+  emitter.on('navigate', function () {
+    if (state.game && state.route !== '/stage/:id') state.game.destroy()
+  })
+}
--- views/home.js
+++ views/home.js
@@ -9,16 +9,30 @@ var font = css`
 `
 var heading = css`
   :host {
-    text-shadow: #aaa 5px 3px 18px;
+    text-shadow: #aaa 5px 3px 18px,
+      -1px -1px 0 #137752,
+       1px -1px 0 #137752,
+      -1px  1px 0 #137752,
+       1px  1px 0 #137752;
+    -webkit-text-stroke: 1px #137752;
+    -webkit-text-fill-color: white;
+  }
+`
+var border = css`
+  :host {
+    -webkit-box-shadow: 0px 0px 0px 4px #fff;
+    -moz-box-shadow: 0px 0px 0px 4px #fff;
+    box-shadow: 0px 0px 0px 1rem #ff6300;
   }
 `
 module.exports = function (state, emit) {
   return html`
-  <main class="cf w-100 pa2-ns green ${font} ${heading}">
-    <h1 class="tc f1">Level Select</h1>
+  <main class="${font} cf w-100 pa2-ns bg-light-blue">
+    <h1 class="${heading} tc f1 white">Level Select</h1>
     <article class="fl w-100 w-50-m  w-25-ns pa2-ns">
-      <div class="aspect-ratio aspect-ratio--1x1 ba bw4 b--orange br4">
-        <div class="ba bw4 b--gold br4 bg-gold">
+      <div class="aspect-ratio aspect-ratio--1x1 br4 ma3 ${border}">
+        <div class="aspect-ratio aspect-ratio--1x1 ba bw4 b--gold br4 bg-gold">
+          <p class="f-headline tc w-100">1</p>
         </div>
       </div>
       <a href="/stage/demo" class="ph2 ph0-ns pb3 link db">
