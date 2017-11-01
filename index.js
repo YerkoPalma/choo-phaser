e2e0f18257d3fc81dd7b08ad41c5d8ef7297aee4
YerkoPalma <yerko.palma@usach.cl>
2017-11-01 17:37:15 -0300

    more sprites

--- index.js
+++ index.js
@@ -9,7 +9,6 @@ css('tachyons', { global: true })
 var app = choo()
 if (process.env.NODE_ENV !== 'production') {
   app.use(require('choo-devtools')())
-  // app.use(require('choo-log')())
 }
 app.use(tts)
 app.use(clearGame)
